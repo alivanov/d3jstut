47c403c24b6f958e4b530fa014a7614668a5b57d
Alexey Ivanov <ivanov@intspirit.com>
2013-09-24 15:09:49 +0400

    axis

--- d3app.js
+++ d3app.js
@@ -1,21 +1,16 @@
-var dataset = [
-                  [ 5,     20 ],
-                  [ 480,   90 ],
-                  [ 250,   50 ],
-                  [ 100,   33 ],
-                  [ 330,   95 ],
-                  [ 410,   12 ],
-                  [ 475,   44 ],
-                  [ 25,    67 ],
-                  [ 85,    21 ],
-                  [ 220,   88 ],
-                  [ 600,   150]
-              ];
+//Dynamic, random dataset
+var dataset = [];
+var numDataPoints = 50;
+for (var i = 0; i < numDataPoints; i++) {
+    var newNumber1 = Math.random();
+    var newNumber2 = Math.random();
+    dataset.push([newNumber1, newNumber2]);
+}
 
 //Width and height
 var w = 800;
 var h = 500;
-var padding = 20;
+var padding = 50;
 
 var svg = d3.select("body")
             .append("svg")
@@ -32,7 +27,22 @@ var yScale = d3.scale.linear()
 
 var rScale = d3.scale.linear()
      .domain([0, d3.max(dataset, function(d) { return d[1]; })])
-     .range([2, 15]);
+     .range([2, 5]);
+
+var formatAsPercentage = d3.format(".1%");
+
+var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .ticks(5)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left")
+    .ticks(5);
+
+xAxis.tickFormat(formatAsPercentage);
+yAxis.tickFormat(formatAsPercentage);
 
 svg.selectAll("circle")
    .data(dataset)
@@ -48,7 +58,7 @@ svg.selectAll("circle")
         return rScale(d[1]);
     });
 
-svg.selectAll("text")
+/*svg.selectAll("text")
    .data(dataset)
    .enter()
    .append("text")
@@ -63,4 +73,14 @@ svg.selectAll("text")
    })
    .attr("font-family", "sans-serif")
    .attr("font-size", "11px")
-   .attr("fill", "red");
+   .attr("fill", "red");*/
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + (h - padding) + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(" + padding + ",0)")
+    .call(yAxis);
